d13624fbf3ce04eb03777119cdda59b21f14f592
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2026-07-13 20:36:58 +0200

    Define a public API

--- src/scrapyrus/__init__.py
+++ src/scrapyrus/__init__.py
@@ -1,4 +1,17 @@
 from importlib import metadata
 
+from scrapyrus.metadata.base import catalog, table_summary
+from scrapyrus.transcriptions.core import (
+    epidoc_xml_to_text,
+    translation_epidoc_xml_to_text,
+)
+
 __version__ = metadata.version(__package__)
-del metadata
+
+__all__ = [
+    "__version__",
+    "catalog",
+    "epidoc_xml_to_text",
+    "table_summary",
+    "translation_epidoc_xml_to_text",
+]
